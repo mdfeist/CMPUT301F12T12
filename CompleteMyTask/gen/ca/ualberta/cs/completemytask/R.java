0acd8618109474491ce1ba14a61dcefea2a5ac0c
Devon Waldon <devon@Laptop-Devon.(none)>
2012-10-23 13:15:36 -0600

    Made main menu a listview (instead of tableview) Created backup view for table-oriented menu

--- CompleteMyTask/gen/ca/ualberta/cs/completemytask/R.java
+++ CompleteMyTask/gen/ca/ualberta/cs/completemytask/R.java
@@ -16,15 +16,17 @@ public final class R {
     }
     public static final class id {
         public static final int AddTaskButton=0x7f070001;
-        public static final int MyTasksHeader=0x7f070002;
-        public static final int MyTasksTable=0x7f070003;
-        public static final int PublicTasksHeader=0x7f070004;
-        public static final int PublicTasksTable=0x7f070005;
-        public static final int menu_settings=0x7f070006;
-        public static final int scrollView1=0x7f070000;
+        public static final int MyTasksHeader=0x7f070003;
+        public static final int MyTasksTable=0x7f070004;
+        public static final int PublicTasksHeader=0x7f070005;
+        public static final int PublicTasksTable=0x7f070006;
+        public static final int TasksList=0x7f070000;
+        public static final int menu_settings=0x7f070007;
+        public static final int scrollView1=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main_menu=0x7f030000;
+        public static final int activity_main_menu_tables=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main_menu=0x7f060000;
