d40f5b2d7376c316c2d75ce9d84cfd060be1fd11
Devon Waldon <devon@Laptop-Devon.(none)>
2012-10-23 12:41:40 -0600

    Add Task button

--- CompleteMyTask/gen/ca/ualberta/cs/completemytask/R.java
+++ CompleteMyTask/gen/ca/ualberta/cs/completemytask/R.java
@@ -15,11 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int MyTasksHeader=0x7f070001;
-        public static final int MyTasksTable=0x7f070002;
-        public static final int PublicTasksHeader=0x7f070003;
-        public static final int PublicTasksTable=0x7f070004;
-        public static final int menu_settings=0x7f070005;
+        public static final int AddTaskButton=0x7f070001;
+        public static final int MyTasksHeader=0x7f070002;
+        public static final int MyTasksTable=0x7f070003;
+        public static final int PublicTasksHeader=0x7f070004;
+        public static final int PublicTasksTable=0x7f070005;
+        public static final int menu_settings=0x7f070006;
         public static final int scrollView1=0x7f070000;
     }
     public static final class layout {
@@ -29,6 +30,7 @@ public final class R {
         public static final int activity_main_menu=0x7f060000;
     }
     public static final class string {
+        public static final int AddTaskButtonLabel=0x7f040006;
         public static final int MyTasksHeader=0x7f040004;
         public static final int PublicTasksHeader=0x7f040005;
         public static final int app_name=0x7f040000;
