e214401f1dcabb03a6fc7f5e93e81ffcc3cb673c
mdfeist <mikefeist@.ualberta.ca>
2012-10-28 17:22:57 -0600

    Only trys to save task if the task is local

--- CompleteMyTask/src/ca/ualberta/cs/completemytask/CommentActivity.java
+++ CompleteMyTask/src/ca/ualberta/cs/completemytask/CommentActivity.java
@@ -87,8 +87,10 @@ public class CommentActivity extends Activity {
 			comment.setUser(user);
 			task.addComment(comment);
 			
-			File localDataFile = new File(getFilesDir(), Settings.DATA_NAME);
-			TaskManager.getInstance().saveLocalData(localDataFile);
+			if (task.isLocal()) {
+				File localDataFile = new File(getFilesDir(), Settings.DATA_NAME);
+				TaskManager.getInstance().saveLocalData(localDataFile);
+			}
 			
 			String commentsString = commentsTextView.getText().toString();
 			commentsString +=  user.getUserName() + "\n\n\t" + comment.getContent() + "\n\n\n";
