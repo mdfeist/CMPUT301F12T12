144208bd01fed8f30ae470a33a143dd8afdaed0e
mdfeist <mikefeist@.ualberta.ca>
2012-10-27 20:44:40 -0600

    Added local file saving
    + loadLocalData(file : File)
    + saveLocalData(file : File)

--- CompleteMyTask/src/ca/ualberta/cs/completemytask/TaskManager.java
+++ CompleteMyTask/src/ca/ualberta/cs/completemytask/TaskManager.java
@@ -1,5 +1,17 @@
 package ca.ualberta.cs.completemytask;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.util.*;
 
 import android.util.Log;
@@ -10,7 +22,6 @@ import android.util.Log;
  * @author Michael Feist
  *
  */
-
 public class TaskManager {
 	
 	private static TaskManager instance = null;
@@ -25,6 +36,11 @@ public class TaskManager {
 		this.loadedData = false;
 	}
 
+	/**
+	 * Returns the singleton's instance.
+	 * 
+	 * @return instances
+	 */
 	public static TaskManager getInstance() {
 		if (instance == null) {
 			instance = new TaskManager();
@@ -32,10 +48,20 @@ public class TaskManager {
 		return instance;
 	}
 	
+	/**
+	 * Returns the number of tasks.
+	 * 
+	 * @return number of tasks
+	 */
 	public int size() {
 		return this.tasks.size();
 	}
 	
+	/**
+	 * Add a task to the task manager.
+	 * 
+	 * @param task
+	 */
 	public void addTask(Task task) {
 		this.tasks.add(task);
 		
@@ -43,56 +69,81 @@ public class TaskManager {
 		Collections.sort(this.tasks, comparator);
 	}
 	
+	/**
+	 * Returns the task at the given position.
+	 * 
+	 * @param position
+	 * @return task
+	 */
 	public Task getTaskAt(int position) {
 		return this.tasks.get(position);
 	}
 	
+	/**
+	 * Returns a list with all the tasks.
+	 * 
+	 * @return List<Task>
+	 */
 	public List<Task> getTaskArray() {
 		return tasks;
 	}
 	
-	public void loadLocalData() {
+	/**
+	 * Loads tasks that were saved locally.
+	 * 
+	 * @param file
+	 */
+	public void loadLocalData(File file) {
 		if (!loadedData) {
 			Log.v(TAG, "Loading local data");
 			
-			createFakeTable();
+			if (file.exists()) {
+				try {
+					// use buffering
+					InputStream fileStream = new FileInputStream(file);
+					InputStream buffer = new BufferedInputStream(fileStream);
+					ObjectInput input = new ObjectInputStream(buffer);
+					try {
+						// deserialize the List
+						@SuppressWarnings("unchecked")
+						List<Task> readObject = (List<Task>) input.readObject();
+						this.tasks.addAll(readObject);
+					} finally {
+						input.close();
+					}
+				} catch (ClassNotFoundException e) {
+					Log.w("ExternalStorage", "Cannot perform input. Class not found. "
+							+ file, e);
+				} catch (IOException e) {
+					Log.w("ExternalStorage", "Cannot perform input: " + file, e);
+				}
+			}
+
 			this.loadedData = true;
 		}
 	}
 	
-	private void createFakeTable() {
-    	for (int i = 0; i < 2; i ++) {
-	    	
-	        // create a new Task
-	        Task task = new Task("My Task " + i, "");
-	        
-	        TaskManager.getInstance().addTask(task);
-	        
-    	}
-    	/*
-    	for (int i = 0; i < 5; i ++) {
-	    	
-	        // create a new Task
-	        Task task = new Task("Public Task " + i, "");
-	        task.setShared(true);
-	        
-	        TaskManager.getInstance().addTask(task);
-	        
-    	}
-    	*/
-    }
-	/*
-  
-   file = new File(getFilesDir(), "batteryData");
-   
-	public void save(File file) {
+	/**
+	 * Save tasks to local storage.
+	 * 
+	 * @param file
+	 */
+	public void saveLocalData(File file) {
+		List<Task> saveTasks = new ArrayList<Task>();
+		
+		for (Task t : tasks) {
+			if (t.isLocal()) {
+				saveTasks.add(t);
+			}
+		}
+		
 		try {
 			// use buffering
 			OutputStream fileStream = new FileOutputStream(file);
 			OutputStream buffer = new BufferedOutputStream(fileStream);
 			ObjectOutput output = new ObjectOutputStream(buffer);
 			try {
-				output.writeObject(this.taskList);
+				output.writeObject(saveTasks);
 			} finally {
 				output.close();
 			}
@@ -102,26 +153,24 @@ public class TaskManager {
 		}
 	}
 	
-	public void load(File file) {
-		try {
-			// use buffering
-			InputStream fileStream = new FileInputStream(file);
-			InputStream buffer = new BufferedInputStream(fileStream);
-			ObjectInput input = new ObjectInputStream(buffer);
-			try {
-				// deserialize the List
-				@SuppressWarnings("unchecked")
-				List<Task> readObject = (List<Task>) input.readObject();
-				this.taskList = readObject;
-			} finally {
-				input.close();
-			}
-		} catch (ClassNotFoundException e) {
-			Log.w("ExternalStorage", "Cannot perform input. Class not found. "
-					+ file, e);
-		} catch (IOException e) {
-			Log.w("ExternalStorage", "Cannot perform input: " + file, e);
-		}
-	}
-	*/
+	public void createFakeTable(int local, int global) {
+    	for (int i = 0; i < local; i ++) {
+	    	
+	        // create a new Task
+	        Task task = new Task("My Task " + i, "");
+	        
+	        addTask(task);
+	        
+    	}
+    	
+    	for (int i = 0; i < global; i ++) {
+	    	
+	        // create a new Task
+	        Task task = new Task("Public Task " + i, "");
+	        task.setPublic(true);
+	        
+	        TaskManager.getInstance().addTask(task);
+	        
+    	}
+    }
 }
