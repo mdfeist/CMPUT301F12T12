5c081ef795025a65eac8d5b4342d6f46d60e1a22
dwaldon <dwaldon@ualberta.ca>
2012-10-30 13:57:39 -0600

    Changed Image from camera operations

--- CompleteMyTask/src/ca/ualberta/cs/completemytask/ViewImageActivity.java
+++ CompleteMyTask/src/ca/ualberta/cs/completemytask/ViewImageActivity.java
@@ -24,8 +24,8 @@ import android.widget.Gallery;
 public class ViewImageActivity extends Activity {
 
 	private static final int CAPTURE_IMAGE_REQUEST_CODE = 100;
+	private static final String TAG = "ViewImageActivity";
 	Uri imageFileUri;
-	Bitmap newPhoto;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -43,26 +43,27 @@ public class ViewImageActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_view_image, menu);
 		return true;
 	}
-
-	/**
-	 * Adds a new photo to the task
-	 */
-	public void addPhoto(View view){
-		MyPhoto image = new MyPhoto();
-
-		takePhoto();
-
-		if (newPhoto!=null){
+	
+	public void takePhoto(View view){
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST_CODE);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		Bundle extras = data.getExtras();
+	    Bitmap newPhoto = (Bitmap) extras.get("data");
+	    
+	    if (newPhoto!=null){
+			MyPhoto image = new MyPhoto();
 			image.setContent(newPhoto);
 			int taskPosition = TaskManager.getInstance().getCurrentTaskPosition();
 			TaskManager.getInstance().getTaskAt(taskPosition).addPhoto(image);
-			Log.v("ViewImage",getString(TaskManager.getInstance().getTaskAt(taskPosition).getNumberOfPhotos()));
-			newPhoto=null;
+			Log.v(TAG,getString(TaskManager.getInstance().getTaskAt(taskPosition).getNumberOfPhotos()));
 		}
 	}
 
-	public void takePhoto(){
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	/*public void takePhoto(){
+		Int	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
 		String folder = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp";
 		File folderF = new File(folder);
@@ -84,7 +85,29 @@ public class ViewImageActivity extends Activity {
 				newPhoto = BitmapFactory.decodeFile(imageFileUri.getPath());
 			}
 		}
+	}ent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		String folder = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp";
+		File folderF = new File(folder);
+		if (!folderF.exists()) {
+			folderF.mkdir();
+		}
+
+		String imageFilePath = folder + "/" + String.valueOf(System.currentTimeMillis()) +".jpg";
+		File imageFile = new File(imageFilePath);
+		imageFileUri = Uri.fromFile(imageFile);
+
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+		startActivityForResult(intent, CAPTURE_IMAGE_REQUEST_CODE);
 	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		if (requestCode == CAPTURE_IMAGE_REQUEST_CODE) {
+			if (resultCode == RESULT_OK){    			
+				newPhoto = BitmapFactory.decodeFile(imageFileUri.getPath());
+			}
+		}
+	}*/
 
 	/**
 	 * Closes the task
