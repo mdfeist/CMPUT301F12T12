5bc6570545d3493a3d61c33133a24969192a72df
mdfeist <mikefeist@.ualberta.ca>
2012-11-06 12:53:47 -0700

    Added a simple way to run a tasks in the background

---
+++ CompleteMyTask/src/ca/ualberta/cs/completemytask/BackgroundTask.java
@@ -0,0 +1,41 @@
+package ca.ualberta.cs.completemytask;
+
+import android.os.AsyncTask;
+
+public class BackgroundTask {
+	
+	public BackgroundTask() {
+		
+	}
+	
+	public void runInBackGround(final HandleInBackground data) {
+		class BackgroundAsyncTask extends AsyncTask<String, Void, Long>{
+    		
+    		@Override
+    		protected void onPreExecute() {
+    			data.onPreExecute();
+    		}
+    		
+	        @Override
+	        protected Long doInBackground(String... params) {
+	        	return (long) data.handleInBackground(null);
+			}
+	        
+	        @Override
+	        protected void onProgressUpdate(Void... voids) {
+	        	
+	        }
+	        
+	        @Override
+	        protected void onPostExecute(Long result) {
+	            super.onPostExecute(null);
+	            
+	            data.onPostExecute();
+	        }        
+		}
+		
+		BackgroundAsyncTask run = new BackgroundAsyncTask();
+		run.execute(); 
+	}
+
+}
